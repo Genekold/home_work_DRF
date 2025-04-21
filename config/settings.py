151965879f7a8a0b089572057115b341c370e7b6
mihaylovea <mihailovevgenya@gmail.com>
2025-04-21 18:37:03 +0300

    add model User

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'debug_toolbar',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -94,3 +95,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
