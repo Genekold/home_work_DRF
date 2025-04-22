97310aafad02110c4528f1852e62dc1a7e07724e
mihaylovea <mihailovevgenya@gmail.com>
2025-04-22 23:23:00 +0300

    add CRUD ViewSet, generic

---
+++ lms/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from lms.apps import LmsConfig
+from lms.views import (CourseViewSet, LessonCreateApiView,
+                       LessonDestroyApiView, LessonListApiView,
+                       LessonRetrieveApiView, LessonUpdateApiView)
+
+app_name = LmsConfig.name
+
+router = SimpleRouter()
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path("lessons/", LessonListApiView.as_view(), name="lesson-list"),
+    path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson-retrieve"),
+    path("lesson/create/", LessonCreateApiView.as_view(), name="lesson-create"),
+    path(
+        "lesson/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lesson-delete"
+    ),
+    path(
+        "lesson/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lesson-update"
+    ),
+] + router.urls
