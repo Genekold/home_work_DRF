7065b6cd0a2f50d945f9fcf715e268bb1770ec23
main <git@github.com:Genekold/course_projekt_OOP.git>
2025-05-02 15:43:26 +0300

    permission auth

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import filters
 from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
@@ -9,6 +10,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class UsersCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
